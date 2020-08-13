5cffbf176e458b5c8a41e65c349e08add4872ba2
Gitanshu <gitanshugupta7@gmail.com>
2020-08-13 17:06:14 +0530

    added routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HerosectionComponent } from './mainview/herosection/herosection.component';
+import { ClassesComponent } from './mainview/classes/classes.component';
+import { FeatureComponent } from './mainview/feature/feature.component';
+import { ContactusComponent } from './mainview/contactus/contactus.component';
+import { AboutusComponent} from './mainview/aboutus/aboutus.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: HerosectionComponent, },
+  { path: 'classes', component: ClassesComponent, },
+  { path: 'features', component: FeatureComponent, },
+  { path: 'contact', component: ContactusComponent, },
+  { path: 'about', component: AboutusComponent, },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
